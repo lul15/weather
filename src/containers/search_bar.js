7263fa009e6a8f59dbeee99989004b80c981c174
external_wifi <external_wifi@LLs-MacBook-Pro.local>
2018-01-08 13:57:40 -0600

    lesson 59 - redux promise in practice

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   //component state
   constructor(props) {
     super(props);
     this.state = {term: ''};
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this); 
   }
 
   onInputChange(event) {
@@ -17,6 +21,9 @@ export default class SearchBar extends Component {
     event.preventDefault();
 
     //need to fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
+
   }
 
   render() {
@@ -35,3 +42,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps) (SearchBar);

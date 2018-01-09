aae98418414d6ff594fadcc179aedfe0e0df13a5
external_wifi <external_wifi@LLs-MacBook-Pro.local>
2018-01-09 08:44:53 -0600

    lesson 60 - avoid state mutations in reducers

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,10 @@
-export default function(state = null, action) {
-  console.log('Action received', action);
-  return state; 
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function(state = [], action) {
+  switch(action.type) {
+    case FETCH_WEATHER:
+      /*return state.concat([action.payload.data]);*/ //concat creates a new state, similar to line below
+      return [action.payload.data, ...state]; 
+  }
+  return state;
 }
